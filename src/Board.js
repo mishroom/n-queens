7d38e8904cf632c0975ebead4d483576896e4db1
misha <mishall.afzal@gmail.com>
2017-12-07 09:40:18 -0800

    minor diagonal half working

--- src/Board.js
+++ src/Board.js
@@ -232,33 +232,21 @@
       var index = minorDiagonalColumnIndexAtFirstRow;
       var matrix = this.rows();
       var diagArr = [];
-      var c =  (matrix.length) +2 - index;
-      console.log("index",index, 'c', c);
-      // console.log(matrix);
-        if (index < matrix.length && c >= 0) {
-        for (var r = 0; r < matrix.length; r++) {
-          
+      var c = index;
+      
+      for (var r = 0; r < matrix.length; r++) {
+        if(c<matrix.length) {
           diagArr.push(matrix[r][c]);
-          // for (var c = matrix.length - 1; c >= 0; c--) {
-          //   if (matrix[r][c]) {
-          //     diagArr.push(matrix[r][c]);
-          //     console.log(diagArr);
-          //   }
-          c--;
-          //}
         }
-        // console.log(diagArr);
-        var count = 0;
-        for (var i = 0; i < diagArr.length; i++) {
-          if (diagArr[i] === 1) {
-            count ++;
-          }
-        }
-        if (count > 1) {
+        c--;
+      }
+      if(diagArr.length){
+        if (diagArr.reduce(function (a,b){
+          return a+b;
+        }) > 1) {
           return true;
         }
       }
-
 
       return false; // fixme
     },
